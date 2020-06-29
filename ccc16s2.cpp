151a5dc3fd253e9b818633b92916b00dbdfd865b
qwertytown4life <62229107+qwertytown4life@users.noreply.github.com>
2020-06-29 16:24:29 -0400

    Create ccc16s2.cpp

---
+++ ccc16s2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> //(DMOJ ONLY)
+using namespace std;
+
+int a[105], b[105];
+
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+
+    int type, N, sum = 0;
+    cin >> type;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> b[i];
+    }
+
+    sort(a,a+N);
+    sort(b, b + N);
+
+    if (type == 2) {
+        reverse(b, b + N);
+    }
+
+    for (int i = 0; i < N; i++) {
+        sum += max(a[i],b[i]);
+    }
+
+    cout << sum << '\n';
+    return 0;
+}
