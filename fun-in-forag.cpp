075a478fccea607475dfa5643904f2971820373a
qwertytown4life <62229107+qwertytown4life@users.noreply.github.com>
2020-07-05 01:23:35 -0400

    Create fun-in-forag.cpp

---
+++ fun-in-forag.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+//I don't think this should pass :blobsweats:
+
+bool dij(vector<tuple<int, int, int>> graph[200003], int nodes, int begin, int end, int K, int time) {
+    vector<int> dist (nodes+1,INT_MAX);
+    deque<int> q;
+    dist.at(begin) = 0;
+    q.push_front(begin);
+    int cur;
+    int inq[200003];
+    inq[begin] = 1;
+    while (!q.empty()) {
+        cur = q.front();
+        q.pop_front();
+        inq[cur] = 0;
+        for (tuple<int, int, int> thing : graph[cur]) {
+            if (get<0>(thing) <= K) {
+                int distance = dist[cur] + get<2>(thing);
+                if (dist[get<1>(thing)] > distance) {
+                    dist[get<1>(thing)] = distance;
+                    if (!inq[get<1>(thing)]) {
+                        q.push_back(get<1>(thing));
+                        inq[get<1>(thing)] = 1;
+                    }
+                }
+            }
+        }
+    }
+    return dist[end] < time;
+}
+
+int N, M, start, End, minutes;
+vector<tuple<int, int, int>> graph[200003];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for (int i = 1; i <= M; i++) {
+        int a, b, w;
+        cin >> a >> b >> w;
+        graph[a].emplace_back(i,b,w);
+        graph[b].emplace_back(i,a,w);
+    }
+
+    cin >> start >> End >> minutes;
+    int lo = 0, hi = M;
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (dij(graph, N, start, End, mid, minutes)) {
+            hi = mid - 1;
+        }
+        else (lo = mid + 1);
+    }
+
+    if (lo <= M) {
+        cout << lo << '\n';
+    }
+    else (cout << "-1" << '\n');
+    return 0;
+}
