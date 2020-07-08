9bf5133cf68a23aadc379877749a6aee03e52ed4
qwertytown4life <62229107+qwertytown4life@users.noreply.github.com>
2020-07-08 11:27:34 -0400

    Create ccc20s2fast.py

---
+++ ccc20s2fast.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+raw_input = sys.stdin.readline
+N = int(raw_input())
+M = int(raw_input())
+MAX = 1000005
+vis = [0] * MAX
+graph = [[] for i in range(MAX)]
+
+def bfs(cur):
+    q = deque()
+    q.append(cur)
+    while q:
+        where = q.popleft()
+        if where == M * N:
+            return 'yes'
+        for i in graph[where]:
+            if not vis[i]:
+                q.append(i)
+                vis[i] = 1
+    return 'no'
+
+for i in range(1, N + 1, 1):
+    k = i
+    row = list(map(int, raw_input().split()))
+    for j in range(1, M + 1, 1):
+        graph[k].append(row[j - 1])
+        k += i
+
+print bfs(1)
