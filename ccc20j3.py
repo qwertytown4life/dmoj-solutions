e1e3de694902e0a0103c626bfdd7dec7bec06b86
qwertytown4life <62229107+qwertytown4life@users.noreply.github.com>
2020-07-14 12:14:16 -0400

    Update ccc20j3.py

--- ccc20j3.py
+++ ccc20j3.py
@@ -14,24 +14,18 @@ while amount_of_coords != 1:
 
     if XQ < smallestX:
         smallestX = XQ
-    else:
-        smallestX = smallestX
-
+ 
     if YQ < smallestY:
         smallestY = YQ
-    else: smallestY = smallestY
-
-
+    
     if XQ > biggestX:
         biggestX = XQ
-    else:
-        biggestX = biggestX
-
+    
     if YQ > biggestY:
         biggestY = YQ
-    else:
-        biggestY = biggestY
-
+    
     amount_of_coords -= 1
+    
+    
 print(f"{smallestX - 1},{smallestY - 1}")
 print(f"{biggestX + 1},{biggestY + 1}")
